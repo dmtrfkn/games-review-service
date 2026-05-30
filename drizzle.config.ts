bfa63f43606b5ac0633573a3a451fad9f4c51819
Корженко Дмитрий Александрович <korzhenko_d@stilsoft.ru>
2026-05-30 21:21:49 +0700

    Add schemas and routes for server

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,19 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config'
+import { defineConfig } from 'drizzle-kit'
+
+declare const process: {
+  env: {
+    DATABASE_URL?: string
+  }
+}
 
 export default defineConfig({
   schema: './src/server/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!
   },
   verbose: true,
-  strict: true,
-});
+  strict: true
+})
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,107 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  integer,
+  boolean
+} from 'drizzle-orm/pg-core'
+
+export const genres = pgTable('genres', {
+  id: serial('id').primaryKey(),
+
+  name: text('name').notNull().unique(),
+  slug: text('slug').notNull().unique(),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
 
 export const games = pgTable('games', {
   id: serial('id').primaryKey(),
+  externalId: text('external_id').notNull().unique(),
+
   title: text('title').notNull(),
-  score: text('score').notNull(),
+  slug: text('slug').notNull().unique(),
+  posterUrl: text('poster_url'),
   platform: text('platform'),
-});
+
+  subjectiveScore: text('subjective_score').notNull(),
+  metaScore: text('meta_score'),
+  review: text('review'),
+
+  completedAt: timestamp('completed_at').notNull().defaultNow(),
+
+  genreId: integer('genre_id')
+    .notNull()
+    .references(() => genres.id, {
+      onDelete: 'cascade'
+    }),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+
+// Предложка
+
+export const suggestions = pgTable('suggestions', {
+  id: serial('id').primaryKey(),
+  externalId: text('external_id').notNull().unique(),
+
+  gameName: text('game_name').notNull(),
+  posterUrl: text('poster_url'),
+
+  votes: integer('votes').notNull().default(0),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+
+export const suggestionVotes = pgTable('suggestions_votes', {
+  id: serial('id').primaryKey(),
+  suggestionId: integer('suggestion_id')
+    .notNull()
+    .references(() => suggestions.id, {
+      onDelete: 'cascade'
+    }),
+
+  ipHash: text('ip_hash').notNull(),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+
+export const polls = pgTable('polls', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  description: text('description').notNull(),
+
+  votesCount: integer('votes_count').notNull().default(0),
+  isActive: boolean('is_active').notNull().default(true),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+
+export const pollVotes = pgTable('poll_votes', {
+  id: serial('id').primaryKey(),
+  pollId: integer('poll_id')
+    .notNull()
+    .references(() => polls.id, {
+      onDelete: 'cascade'
+    }),
+
+  ipHash: text('ip_hash').notNull(),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+export const admins = pgTable('admins', {
+  id: serial('id').primaryKey(),
+
+  username: text('username').notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
