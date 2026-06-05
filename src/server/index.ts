1144ff0a48acfde43ac792137584e8355c17d661
Корженко Дмитрий Александрович <korzhenko_d@stilsoft.ru>
2026-06-05 12:52:43 +0700

    Add all routes to app

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,20 @@
-import Elysia from 'elysia';
+import Elysia from 'elysia'
+import { authRoutes } from './routes/auth'
+import { genresRoutes } from './routes/genres'
+import { gameRoutes } from './routes/games'
+import { pollRoutes } from './routes/polls'
+import { suggestionRoutes } from './routes/suggestions'
+import { rawgRoutes } from './routes/rawg'
 
-const app = new Elysia();
+const app = new Elysia()
 
-app.get('/api/games', () => {
-  return {
-    games: [{ id: 1, title: 'game 1' }],
-  };
-});
+app.use(authRoutes)
+app.use(genresRoutes)
+app.use(gameRoutes)
+app.use(pollRoutes)
+app.use(suggestionRoutes)
+app.use(rawgRoutes)
 
 app.listen(4200, () => {
-  console.log('Server running on http://localhost:4200');
-});
+  console.log('Server running on http://localhost:4200')
+})
