8f32f78e5699911162ee8c4af2e0bc30e4aa62b9
Корженко Дмитрий Александрович <korzhenko_d@stilsoft.ru>
2026-06-05 12:50:23 +0700

    Add schema to db init for types assertion

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,6 +1,7 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
 
-const sql = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+const sql = postgres(process.env.DATABASE_URL!)
+export const db = drizzle(sql, { schema })
